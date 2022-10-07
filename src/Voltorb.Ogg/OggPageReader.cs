85bfa23cfa910265577e1756b429b7ef71be45a0
Khitiara <robotbrainify@gmail.com>
2022-10-06 20:41:45 -0400

    comment clarification cause why not

--- src/Voltorb.Ogg/OggPageReader.cs
+++ src/Voltorb.Ogg/OggPageReader.cs
@@ -1,6 +1,5 @@
 ﻿using System.Buffers;
 using System.Collections.ObjectModel;
-using System.Diagnostics.CodeAnalysis;
 using System.IO.Hashing;
 using System.Runtime.InteropServices;
 using Nerdbank.Streams;
@@ -30,6 +29,7 @@ public class OggPageReader : IDisposable
     /// Flags specifying whether this page continues a previous packet, as well as whether this page begins or ends a
     /// logical bitstream.
     /// </param>
+    /// <param name="CrcChecksum">The CRC checksum of the Ogg page, per the ogg spec</param>
     /// <param name="SeekPosition">
     /// The absolute position within the source stream of this page, for seeking to pages. This value should generally
     /// not be used by calling code, and exists to support <see cref="OggPageReader.SeekAndReadPageAsync"/>
@@ -43,7 +43,8 @@ public class OggPageReader : IDisposable
     /// that packet's data, possibly requiring even further continuation.
     /// </param>
     public readonly record struct PageInfo(ulong GranulePosition, uint BitstreamSerial, uint PageSequenceNumber,
-        HeaderType PacketType, long SeekPosition, int[] PacketLengths, bool FinalPacketIsComplete);
+        HeaderType PacketType, uint CrcChecksum, long SeekPosition, int[] PacketLengths,
+        bool FinalPacketIsComplete);
 
     /// <summary>
     /// Ogg page data, including metadata recovered from the header as well as a segmented buffer of page data bits
@@ -57,6 +58,7 @@ public class OggPageReader : IDisposable
         public uint BitstreamSerial => PageInfo.BitstreamSerial;
         public uint PageSequenceNumber => PageInfo.PageSequenceNumber;
         public HeaderType PacketType => PageInfo.PacketType;
+        public uint CrcChecksum => PageInfo.CrcChecksum;
         public long SeekPosition => PageInfo.SeekPosition;
         public int[] PacketLengths => PageInfo.PacketLengths;
         public bool FinalPacketIsComplete => PageInfo.FinalPacketIsComplete;
@@ -85,6 +87,11 @@ public class OggPageReader : IDisposable
     private readonly Crc32            _crc;
 
     /// <summary>
+    /// the index of the next page to be read. updated during every call to <see cref="ReadPageAsync"/>
+    /// </summary>
+    private int _currentPageIndex;
+
+    /// <summary>
     /// Allows access to metadata about all pages this reader has encountered.
     /// </summary>
     public IReadOnlyList<PageInfo> SeenPages { get; }
@@ -110,16 +117,24 @@ public class OggPageReader : IDisposable
     public async ValueTask<PageData>
         SeekAndReadPageAsync(int pageIndex, CancellationToken cancellationToken = default) {
         if (pageIndex < _pageOffsetTable.Count) {
+            // we already know where the page is, seek to the start of the capture pattern and read it
             _stream.Seek(_pageOffsetTable[pageIndex].SeekPosition, SeekOrigin.Begin);
-            return await ReadPageAsyncCore(pageIndex, cancellationToken);
+            _currentPageIndex = pageIndex;
+            return await ReadPageAsync(cancellationToken: cancellationToken);
         }
 
-        _pageOffsetTable.EnsureCapacity(pageIndex);
+        // no record of the page, start reading pages
+        _pageOffsetTable.EnsureCapacity(pageIndex + 1);
+        // go to the last page we know (just in case), and bypass its header to prevent off-by-one error
+        _stream.Seek(_pageOffsetTable[^1].SeekPosition + 27, SeekOrigin.Begin);
+        _currentPageIndex = _pageOffsetTable.Count;
+        // read in-between pages
         for (int i = _pageOffsetTable.Count; i < pageIndex; i++) {
-            await ReadPageAsyncCore(i, cancellationToken);
+            // make sure we dispose the data sequence when 
+            (await ReadPageAsync(cancellationToken: cancellationToken)).Dispose();
         }
 
-        return await ReadPageAsyncCore(pageIndex, cancellationToken);
+        return await ReadPageAsync(cancellationToken: cancellationToken);
     }
 
 
@@ -131,36 +146,74 @@ public class OggPageReader : IDisposable
     /// If re-reading a previously read page after a seek operation, the page header will still be re-read and metadata
     /// updated, as well as the checksum re-verified for the page.
     /// </summary>
-    public ValueTask<PageData> ReadPageAsync(CancellationToken cancellationToken = default) =>
-        ReadPageAsyncCore(_pageOffsetTable.Count, cancellationToken);
-
-    [SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods")]
-    private async ValueTask<PageData> ReadPageAsyncCore(int idx, CancellationToken cancellationToken = default) {
+    public async ValueTask<PageData> ReadPageAsync(bool validateCrc = true,
+        CancellationToken cancellationToken = default) {
         // grab a buffer for the page data. this will be used for calculating the crc and then sliced 
         Sequence<byte> sequenceForCrc = new(_memoryPool);
+        // write in the capture pattern since its included in the CRC
+        sequenceForCrc.Write(OggConstants.CapturePattern);
 
-        // get a segment for reading the header into
+        // get a segment for reading the header into. minimum size 23 so once we get the capture pattern we can
+        // overwrite that and get an exact-size buffer
         Memory<byte> memory = sequenceForCrc.GetMemory(23);
-        int indexOf;
-        int count;
+        memory.Span.Fill(0);
+
+        // one of three scenarios can happen after each iteration of the loop:
+        // - capture pattern found in full. copy moves later bytes over top of the capture pattern
+        //      and a second read is required before decoding
+        // - capture pattern not in read buffer at all. copy last three bytes to the start and loop again
+        // - up to three bytes of capture pattern at end of read buffer but cut off. copy last three bytes to the start
+        //      and remaining capture pattern bytes will be placed contiguously after. after one loop, capture pattern
+        //      is de-fragmented, will be detected with index < 3, continue per first case with possible no second read
+        // first scenario is most likely except when reading forward from mid-page (only possible on start,
+        //      stream error, or manual backing-stream seek by calling code)
+
+        // e.g. for each case
+        // 1: [XXX.......] --Read-> [XXX.OggSYY] -> Break
+        // 2: [XXX.......] --Read-> [XXX....YYY] --NotFound-> [YYY.......] -> Repeat
+        // 3: [XXX.......] --Read-> [XXX.....Og] --NotFound-> [.Og.......] --Read-> [.OggSXXXXX] -> Break
 
         // read until we have the capture pattern in the segment somewhere
         // in sequential operations, this is expected to exit after the first loop iteration
-        do {
-            count = await _stream.ReadAtLeastAsync(memory, OggConstants.CapturePattern.Length,
-                cancellationToken: cancellationToken);
-        } while ((indexOf = memory.Span[..count].IndexOf(OggConstants.CapturePattern)) < 0);
 
-        // slice everything after the capture pattern. since indexof returned >0 on the capture pattern, this will not
-        // overrun, but may return the empty segment. offset will be 0 in that case, and copyto will nop
+        // the last read before breaking from the loop will always end with the capture pattern inside the buffer
+        // taking up 4 bytes so in the best case the first three bytes are pre-filled from previous iteration
+        // and we can read one more capture pattern byte plus 23 main header bytes for a total of a 27-byte buffer.
+        // the initial 3 bytes will never be directly read into from the stream, being kept as holdover from a previous
+        // iteration to detect when the read boundary occurs in the middle of a capture pattern.
+        int cappedBufferSize = Math.Min(27, memory.Length);
+        bool foundCapturePattern;
+        // get a slice for safety on the end range
+        Memory<byte> sliced = memory[..cappedBufferSize];
+        int indexOf;
+        int count = 0;
+        do {
+            // take the last three bytes and move them to the start. copies zeros on first iteration for no effect
+            // first three bytes only exposed to later code if filled with capture pattern by prev. iteration so
+            // the extra zeros will be discarded. use count for the range in case the stream reads less than the full
+            // available buffer. this slice is the same as [3,(count+3)][^3] = [(count+0)..(count+3)]. on the first
+            // run through the loop, count = 0 so this is copying in place and CopyTo will short-circuit cleanly.
+            sliced.Slice(count, 3).CopyTo(memory);
+            // read at least enough bytes for the capture pattern to make the memory shuffle a little easier
+            count = await _stream.ReadAtLeastAsync(sliced[3..], OggConstants.CapturePattern.Length,
+                cancellationToken: cancellationToken);
+            // my new favourite framework method is ReadOnlySpan.IndexOf(ReadOnlySpan)
+            foundCapturePattern = (indexOf = memory.Span[..(count + 3)].IndexOf(OggConstants.CapturePattern)) < 0;
+        } while (!foundCapturePattern);
+
+        // slice everything after the capture pattern. since indexof returned >=0 on the capture pattern, this will not
+        // overrun, but may return an empty slice. offset will be 0 in that case, and we'll overwrite the capture
+        // pattern with the second stream read rather than call into CopyTo
         Memory<byte> slice = memory[(indexOf + OggConstants.CapturePattern.Length)..count];
         int offset = slice.Length;
-        // and move the end back to the start
-        slice.CopyTo(memory);
+        // and move the extra back to the start if there is any
+        if (offset > 0)
+            slice.CopyTo(memory);
 
-        // finish filling the buffer. if we cant fill the buffer thats an error so use ReadExactly
-        await _stream.ReadExactlyAsync(memory[offset..], cancellationToken);
-        // read into the struct (this is a copy)
+        // finish filling the header buffer if we already have enough (offset = remaining after capture pattern >= 23) then dont bother
+        if (offset < 23)
+            await _stream.ReadExactlyAsync(memory[offset..23], cancellationToken);
+        // read into the struct (this makes a copy on the stack)
         PageHeader header = MemoryMarshal.Read<PageHeader>(memory.Span);
         // clear the crc in the buffer now we have a copy in header
         memory.Span[18..21].Fill(0);
@@ -178,7 +231,7 @@ public class OggPageReader : IDisposable
 
         // turn the lacing values (segment lengths) into packet lengths and figure out if we're going to continue
         // a packet on the next page
-        GetPacketLengths(lacingMemory, out int[] packets, out bool lastPacketIsComplete);
+        GetPacketLengths(lacingMemory.Span, out int[] packets, out bool lastPacketIsComplete);
 
         // save the position at the end of the header so we can omit the processed header from the returned data
         SequencePosition dataStartPos = sequenceForCrc.AsReadOnlySequence.End;
@@ -191,30 +244,34 @@ public class OggPageReader : IDisposable
 
         // calculate the Crc32 of the page, with the header crc field set to 0. the _crc should be always reset when
         // this block starts (reset in constructor and in GetHashAndReset), lock on it anyway
-        lock (_crc) {
-            foreach (ReadOnlyMemory<byte> segment in sequenceForCrc.AsReadOnlySequence) {
-                _crc.Append(segment.Span);
-            }
+        if (validateCrc) {
+            lock (_crc) {
+                foreach (ReadOnlyMemory<byte> segment in sequenceForCrc.AsReadOnlySequence) {
+                    _crc.Append(segment.Span);
+                }
 
-            uint crc = 0;
-            _crc.GetHashAndReset(MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref crc, 1)));
-            if (crc != header.CrcChecksum)
-                throw new IOException(); // TODO add an exception message here. is this the right exception type?
+                uint crc = 0;
+                _crc.GetHashAndReset(MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref crc, 1)));
+                if (crc != header.CrcChecksum)
+                    throw new IOException(); // TODO add an exception message here. is this the right exception type?
+            }
         }
 
-        // now we've computed the Crc32 of the page, we dont need the header bytes in our sequence. use AdvanceTo to
+        // now we've checked (or ignored) the Crc32 of the page, we dont need the header bytes in our sequence. use AdvanceTo to
         // skip past them and return segments to the memory pool
         sequenceForCrc.AdvanceTo(dataStartPos);
 
-        // assembly page metadata and add to our offsets table. append is valid even for a strongly indexed list here
-        // because SeekAndReadPageAsync reads in-between pages in order to find future pages, so unless the client
-        // seeks behind our back this is OK
+        // assembly page metadata and add to our offsets table. uses _currentPageIndex for indexing into the page info
+        // list, adding to the end if needed. if the source stream has been seeked behind our back then this may corrupt
+        // the page table, but manually seeking to before the corrupted entry and re-reading pages from there will
+        // restore the setup
         PageInfo pageInfo = new(header.GranulePosition, header.BitstreamSerial, header.PageSequence, header.Flags,
-            streamPosition, packets, lastPacketIsComplete);
-        if (idx < 0 || idx >= _pageOffsetTable.Count)
+            header.CrcChecksum, streamPosition, packets, lastPacketIsComplete);
+        if (_currentPageIndex < 0 || _currentPageIndex >= _pageOffsetTable.Count) {
             _pageOffsetTable.Add(pageInfo);
-        else
-            _pageOffsetTable[idx] = pageInfo;
+            _currentPageIndex++;
+        } else
+            _pageOffsetTable[_currentPageIndex++] = pageInfo;
 
         return new PageData(pageInfo, sequenceForCrc);
     }
@@ -228,7 +285,7 @@ public class OggPageReader : IDisposable
     /// <param name="lacings"></param>
     /// <param name="packets"></param>
     /// <param name="lastPacketIsComplete"></param>
-    private static void GetPacketLengths(Memory<byte> lacings, out int[] packets, out bool lastPacketIsComplete) {
+    private static void GetPacketLengths(ReadOnlySpan<byte> lacings, out int[] packets, out bool lastPacketIsComplete) {
         List<int> lengths = new();
         lastPacketIsComplete = true;
 
@@ -236,8 +293,7 @@ public class OggPageReader : IDisposable
         // with the 0xFF bytes to be added together. an incomplete packet is denoted by the sequence ending without
         // a following smaller lacing byte (packets multiple of 0xFF bytes are denoted by a terminating 0x0 byte)
         int next = 0;
-        for (int i = 0; i < lacings.Length; i++) {
-            byte lacing = lacings.Span[i];
+        foreach (byte lacing in lacings) {
             next += lacing;
             if (lacing == 255) continue;
             lengths.Add(next);
