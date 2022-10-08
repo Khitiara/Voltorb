e107429749d90c49211793520660bce41b045e8c
Khitiara <robotbrainify@gmail.com>
2022-10-07 20:40:55 -0400

    Remove warning comment on ogg constant

--- src/Voltorb.Ogg/OggConstants.cs
+++ src/Voltorb.Ogg/OggConstants.cs
@@ -2,9 +2,7 @@
 
 public static class OggConstants
 {
-    // I'm sorry if your IDE or editor marks this as a syntax error. utf8 string literals are part of c# 11 but not
-    // yet supported by RIDER at least, possibly others. This literal is compiled to a .data entry and no runtime
-    // conversion will take place
+    // This literal is compiled to a .data entry and no runtime conversion will take place
     // See https://github.com/dotnet/csharplang/blob/main/proposals/csharp-11.0/utf8-string-literals.md for more.
     /// <summary>
     /// The Ogg capture pattern bytes, which begin every ogg page
