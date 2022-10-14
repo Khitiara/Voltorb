44a151c781fdd88d775c62592328d2f0637f5ef2
Khitiara <robotbrainify@gmail.com>
2022-10-14 18:36:55 -0400

    test bitstring literals in base 2 for clarity

--- test/Voltorb.Bits.Tests.Common/BitReaderTests.cs
+++ test/Voltorb.Bits.Tests.Common/BitReaderTests.cs
@@ -4,7 +4,7 @@ namespace Voltorb.Bits.Tests.Common;
 
 public class BitReaderTests
 {
-    private readonly byte[] _stream = { 0xFA, 0x23, 0x34, 0x51, 0x25, 0x8f, 0x40, 0x01, 0xf7, };
+    private readonly byte[] _stream = { 0b11111010, 0b00100011, 0b00110100, 0b01010001, 0b00100101, 0b10001111, 0b01000000, 0b00000001, 0b11110111, };
 
     [Fact]
     public void TestReadSimple() {
@@ -31,7 +31,7 @@ public class BitReaderTests
     public void TestPeekAdvance() {
         BitReader bitReader = new(_stream);
         bitReader.TryAdvance(11).Should().BeTrue();
-        bitReader.ReadBits(3).Should().Be((0x23 & 0b111000) >> 3);
+        bitReader.ReadBits(3).Should().Be(0b100);
     }
 
     [Fact]
@@ -56,7 +56,7 @@ public class BitReaderTests
             .WithParameterName("count");
     }
 
-    private static (int, ulong) PeekHelper(BitReader reader, int count) {
+    private static (int count, ulong bits) PeekHelper(BitReader reader, int count) {
         count = reader.PeekBits(count, out var b);
         return (count, b);
     }
@@ -64,8 +64,8 @@ public class BitReaderTests
     [Fact]
     public void TestBitReaderZero() {
         BitReader bitReader = new(_stream);
-        PeekHelper(bitReader, 0).Should().Be((0, 0UL));
-        bitReader.ReadBits(0).Should().Be(0UL);
+        PeekHelper(bitReader, 0).Should().Be((0, 0b0UL));
+        bitReader.ReadBits(0).Should().Be(0b0UL);
         bitReader.TryAdvance(-1).Should().BeTrue();
     }
 
@@ -73,17 +73,17 @@ public class BitReaderTests
     public void TestBitReaderBig() {
         BitReader bitReader = new(_stream);
         bitReader.TryAdvance(5).Should().BeTrue();
-        PeekHelper(bitReader,63).Should().Be((63, 0x380A04792A89A11FUL));
+        PeekHelper(bitReader,63).Should().Be((63, 0b11100000001010000001000111100100101010100010011010000100011111UL));
         bitReader.Position.Should().Be(5);
         bitReader.TryAdvance(1).Should().BeTrue();
         bitReader.TryAdvance(64).Should().BeTrue();
         bitReader.Position.Should().Be(70);
 
         bitReader.Seek(-69, SeekOrigin.Current);
-        bitReader.Position.Should().Be(1);
+        bitReader.Position.Should().Be(0b1);
         bitReader.Seek(5, SeekOrigin.Current);
         bitReader.Seek(-5, SeekOrigin.Current);
-        PeekHelper(bitReader,4).Should().Be((4, 0xDUL));
+        PeekHelper(bitReader,4).Should().Be((4, 0b1101UL));
         bitReader.Seek(1, SeekOrigin.Current);
         bitReader.ReadBit().Should().BeFalse();
     }
